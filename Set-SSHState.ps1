a404535dacb2491cdbfc43f6c1f383af5a990d8e
Sean Broestl <broestls@gmail.com>
2017-02-16 12:30:53 -0500

    Initial commit for Set-SSHState.ps1 script.

---
+++ Set-SSHState.ps1
@@ -0,0 +1,30 @@
+###
+# Set-SSHState.ps1
+#
+# Version: 1.0
+# Author: Sean Broestl
+# Part of collection of PowerCLI scripts at http://github.com/broestls/vmwps/
+#
+# Takes a VMHost and enables SSH if passed the -Enabled parameter, otherwise
+# Disables the SSH service. SSH can be explicitly disabled with the
+# -Disable parameter.
+###
+
+  [cmdletbinding()]
+  param(
+    [Parameter(
+      Mandatory=$True,
+      Position=1,
+      ValueFromPipelineByPropertyName = $true)]
+      [string[]]$Name,
+
+    [switch]$Enable,
+    [switch]$Disable
+  )
+
+  If($Enable) {
+    Get-VMHostService -VMHost (Get-VMHost $Name) | ?{$_.Key -eq "TSM-SSH"} | Start-VMHostService -Confirm:$false
+  }
+  ElseIf(-Not $Enable -Or $Disable) {
+    Get-VMHostService -VMHost (Get-VMHost $Name) | ?{$_.Key -eq "TSM-SSH"} | Stop-VMHostService -Confirm:$false
+  }
